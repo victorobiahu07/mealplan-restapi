fffb649da3e9e4f09cb880e98c9a734f8a092ff4
victorobiahu07 <vick.obiahu@gmail.com>
2019-11-10 23:11:19 -0500

    Update user.go

--- mealplan-restapi/models/user.go
+++ mealplan-restapi/models/user.go
@@ -2,6 +2,8 @@ package models
 
 import "gopkg.in/mgo.v2/bson"
 
+
+//every user has an id and meal plan ID, I included the name property as a bonus
 type User struct {
 	ID     bson.ObjectId `bson:"_id" json:"id"`
 	Name   string        `bson:"name" json:"name"`
